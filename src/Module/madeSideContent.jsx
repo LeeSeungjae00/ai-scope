335cad1e2efcf6621816e924c4e51686e3d89ee0
LeeSeungjae <seungjae2668@gsinstrument.com>
2021-01-29 10:47:42 +0900

    side에서 값 출력하는거 뺴와서 App 으로 변경후 Array 를 전달하는 식으로 변경 Fake 동작도 추가해봄

---
+++ src/Module/madeSideContent.jsx
@@ -0,0 +1,36 @@
+export default function madeSideContent(result) {
+    if (result.length < 3) return [];
+    const resultArray = [];
+
+    result.sort((a, b) => b[Object.keys(b)].value - a[Object.keys(a)].value);
+
+    console.log()
+
+    resultArray.push({
+        title: "Differential diganosis",
+        mainValue: `${Object.keys(result[0])[0]} : ${result[0][Object.keys(result[0])].value}%`,
+        subValue: [
+            `${Object.keys(result[1])[0]} : ${result[1][Object.keys(result[1])].value}%`,
+            `${Object.keys(result[2])[0]} : ${result[2][Object.keys(result[2])].value}%`
+        ]
+    })
+
+    console.log(Object.keys(result[0])[0]);
+
+    if (Object.keys(result[0])[0]=== "EGC") {
+        const { T1a, T1b } = result[0]["EGC"].depth;
+
+        resultArray.push({
+            title: "Depth of invasion in EGC",
+            mainValue: `${T1a > T1b ?
+                "T1a : " + T1a + "%" :
+                "T1b : " + T1b + "%"}`,
+            subValue: [`${T1a < T1b ?
+                "T1a : " + T1a + "%" :
+                "T1b : " + T1b + "%"}`]
+        })
+    }
+
+
+    return resultArray
+}
